a07e550864a83531efd2f024acd1dad1a98f67ea
tylor <info@ettecompany.com>
2022-08-16 14:19:09 +1000

    Feat: ChangeNotifier + 서비스 로직 분리

--- lib/pages/add_medicine/add_alram_page.dart
+++ lib/pages/add_medicine/add_alram_page.dart
@@ -6,16 +6,22 @@ import 'package:app_project/pages/add_medicine/add_medicine_page.dart';
 import 'package:app_project/pages/components/app_widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
+import '../../services/add_medicine_servcie.dart';
 import '../components/add_page_widget.dart';
 
 class AddAlarmPage extends StatelessWidget {
-  const AddAlarmPage(
-      {Key? key, required this.MedicineImage, required this.medicineName})
-      : super(key: key);
+  AddAlarmPage({
+    Key? key,
+    required this.MedicineImage,
+    required this.medicineName,
+  }) : super(key: key);
 
   final File? MedicineImage;
   final String medicineName;
+
+  final service = AddMedicineService();
 
   @override
   Widget build(BuildContext context) {
@@ -30,14 +36,13 @@ class AddAlarmPage extends StatelessWidget {
           const SizedBox(height: largeSpace),
           // Expanded는 칼럼(Coulrm에서 내가 갖고있는 영역 모두에서 그릴 수 있도록 하는 명령어)
           Expanded(
-            child: ListView(
-              children: const [
-                AlramBox(),
-                AlramBox(),
-                AlramBox(),
-                AlramBox(),
-                AddAlramButton(),
-              ],
+            child: AnimatedBuilder(
+              animation: service,
+              builder: (context, _) {
+                return ListView(
+                  children: alarmWidgets,
+                );
+              },
             ),
           ),
         ],
@@ -48,12 +53,33 @@ class AddAlarmPage extends StatelessWidget {
       ),
     );
   }
+
+  List<Widget> get alarmWidgets {
+    final children = <Widget>[];
+    children.addAll(
+      service.alarms.map(
+        (alarmTime) => AlarmBox(
+          time: alarmTime,
+          service: service,
+        ),
+      ),
+    );
+    children.add(AddAlarmButton(
+      service: service,
+    ));
+    return children;
+  }
 }
 
-class AlramBox extends StatelessWidget {
-  const AlramBox({
+class AlarmBox extends StatelessWidget {
+  const AlarmBox({
     Key? key,
+    required this.time,
+    required this.service,
   }) : super(key: key);
+
+  final String time;
+  final AddMedicineService service;
 
   @override
   Widget build(BuildContext context) {
@@ -62,7 +88,9 @@ class AlramBox extends StatelessWidget {
         Expanded(
           flex: 1,
           child: IconButton(
-            onPressed: () {},
+            onPressed: () {
+              service.removeAlarm(time);
+            },
             icon: const Icon(CupertinoIcons.minus_circle),
           ),
         ),
@@ -76,58 +104,14 @@ class AlramBox extends StatelessWidget {
               showModalBottomSheet(
                 context: context,
                 builder: (builder) {
-                  return BottomSheetBody(
-                    children: [
-                      SizedBox(
-                        height: 200,
-                        child: CupertinoDatePicker(
-                          onDateTimeChanged: (dateTime) {},
-                          mode: CupertinoDatePickerMode.time,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: regularSpace,
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: SizedBox(
-                              height: submitButtonHeight,
-                              child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  textStyle:
-                                      Theme.of(context).textTheme.subtitle1,
-                                  primary: Colors.white,
-                                  onPrimary: AppColors.primaryColor,
-                                ),
-                                onPressed: () {},
-                                child: Text('취소'),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: smallSpace,
-                          ),
-                          Expanded(
-                            child: SizedBox(
-                              height: submitButtonHeight,
-                              child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                    textStyle:
-                                        Theme.of(context).textTheme.subtitle1),
-                                onPressed: () {},
-                                child: Text('선택'),
-                              ),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
+                  return TimePickerBottomSheet(
+                    initialTime: time,
+                    service: service,
                   );
                 },
               );
             },
-            child: const Text('20:00'),
+            child: Text(time),
           ),
         ),
       ],
@@ -135,10 +119,86 @@ class AlramBox extends StatelessWidget {
   }
 }
 
-class AddAlramButton extends StatelessWidget {
-  const AddAlramButton({
+// ignore: must_be_immutable
+class TimePickerBottomSheet extends StatelessWidget {
+  TimePickerBottomSheet({
     Key? key,
+    required this.initialTime,
+    required this.service,
   }) : super(key: key);
+
+  final String initialTime;
+  final AddMedicineService service;
+  DateTime? _setDateTime;
+
+  @override
+  Widget build(BuildContext context) {
+    final initialDateTime = DateFormat('HH:mm').parse(initialTime);
+
+    return BottomSheetBody(
+      children: [
+        SizedBox(
+          height: 200,
+          child: CupertinoDatePicker(
+            onDateTimeChanged: (dateTime) {
+              _setDateTime = dateTime;
+            },
+            mode: CupertinoDatePickerMode.time,
+            initialDateTime: initialDateTime,
+          ),
+        ),
+        const SizedBox(
+          height: regularSpace,
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: SizedBox(
+                height: submitButtonHeight,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    textStyle: Theme.of(context).textTheme.subtitle1,
+                    primary: Colors.white,
+                    onPrimary: AppColors.primaryColor,
+                  ),
+                  onPressed: () => Navigator.pop(context),
+                  child: Text('취소'),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: smallSpace,
+            ),
+            Expanded(
+              child: SizedBox(
+                height: submitButtonHeight,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      textStyle: Theme.of(context).textTheme.subtitle1),
+                  onPressed: () {
+                    service.setAlarm(
+                        prevTime: initialTime,
+                        setTime: _setDateTime ?? initialDateTime);
+                    Navigator.pop(context);
+                  },
+                  child: Text('선택'),
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class AddAlarmButton extends StatelessWidget {
+  const AddAlarmButton({
+    Key? key,
+    required this.service,
+  }) : super(key: key);
+
+  final AddMedicineService service;
 
   @override
   Widget build(BuildContext context) {
@@ -147,7 +207,7 @@ class AddAlramButton extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 6),
         textStyle: Theme.of(context).textTheme.subtitle1,
       ),
-      onPressed: () {},
+      onPressed: service.addNowAlarm,
       child: Row(
         children: const [
           Expanded(
